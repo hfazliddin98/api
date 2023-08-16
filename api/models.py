3ae87f8eb960fedd900f18bf65cb02fb3815019a
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-08-16 10:06:22 +0500

    server

--- api/models.py
+++ api/models.py
@@ -3,14 +3,6 @@ from django.db import models
 class Fayl(models.Model):
     ism = models.CharField(max_length=100)
     familya = models.CharField(max_length=100)
-    fan = models.CharField(max_length=100)
-    pasport = models.ImageField(upload_to='pasportlar/', null = True)
-    diplom = models.FileField(upload_to='diplomlar/')
-    ariza = models.FileField(upload_to='ariza/')
-    malumotnoma = models.FileField(upload_to='malumotnoma/')
-    anketa = models.FileField(upload_to='minimum anketa/')
-    kochirma = models.FileField(upload_to='kengash ko`chirmasi/')
-    yollanma = models.FileField(upload_to='yo`lanma xat/')
-    royhat = models.FileField(upload_to='ilmiy ishlar ro`yati/')
+    fan = models.CharField(max_length=100)    
     sana = models.DateTimeField(auto_now_add=True)
     
