0ec64c4c86fdca844d51512a85b85e91da4e1bc3
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-08-21 09:48:08 +0500

    baza

--- asosiy/settings.py
+++ asosiy/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'asosiy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'api',
-#         'USER': 'apiuser',
-#         'PASSWORD': 'Hfazliddin98',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'api',
+        'USER': 'apiuser',
+        'PASSWORD': 'Hfazliddin98',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
